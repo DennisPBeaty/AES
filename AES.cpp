c7bcbd7cfb347558b790d8caa9fe201f9777ad4c
DennisPBeaty <83377610+DennisPBeaty@users.noreply.github.com>
2022-09-11 14:20:37 -0400

    Update AES.cpp

--- AES.cpp
+++ AES.cpp
@@ -93,60 +93,106 @@ uint32_t rotWord(uint32_t word)
 }
 
 // This transformation substitutes each byte in the State with its corresponding value from the S-Box.
-void subBytes(uint32_t **state)
+uint8_t ** subBytes(uint8_t **state)
 {
+    uint8_t **buffer;
+    
+    //Loop through every byte within the state
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            //Find the correct substitution within the sbox
+            uint8_t current = state[i][j];
 
+            int k = (current & 0xF0) >> 4;
+            int l = (current & 0xF);
+
+            //Insert value to buffer
+            buffer[i][j] = sBox[k][l];
+        }
+    }
+
+    return buffer;
 }
 
-// This transformation substitutes each byte in the State with its corresponding value from the S-Box.
-void shiftRows(uint32_t **state)
+// This transformation performs a circular shift on each row in the State (see Section 5.1.2)
+uint8_t ** shiftRows(uint8_t **state)
 {
+    uint8_t **buffer;
 
-}
-
-// This transformation substitutes each byte in the State with its corresponding value from the S-Box.
-void subBytes(uint32_t **state)
-{
-
-}
-
-// This transformation performs a circular shift on each row in the State
-void shiftRows(uint32_t **state)
-{
-
+    //Loop through every row within the state
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            //Create buffer for each element in array
+            uint8_t temp1 = state[i][0];
+            uint8_t temp2 = state[i][1];
+            uint8_t temp3 = state[i][2];
+            uint8_t temp4 = state[i][3];
+            //Rotate each element to the left
+            state[i][0] = temp2;
+            state[i][1] = temp3;
+            state[i][2] = temp4;
+            state[i][3] = temp1;
+        }
+    }
 }
 
 // This transformation treats each column in state as a four-term polynomial. This polynomial is multiplied 
 // (modulo another polynomial) by a fixed polynomial with coefficients (see Sections 4.3 and 5.1.3).
-void mixColumns(uint32_t **state)
+void mixColumns(uint8_t **state)
 {
+    uint8_t state0, state1, state2, state3;
 
+    //Loop through every column within the state
+    for (int i = 0; i < 4; i++)
+    {
+        //First Row Value Change
+        state0 = ffadd(ffadd(ffadd(ffmultiply(0x02, state[0][i]), ffmultiply(0x03, state[1][i])), state[2][i]), state[3][i]);
+        //Second Row Value Change
+        state1 = ffadd(ffadd(ffadd(ffmultiply(0x02, state[1][i]), ffmultiply(0x03, state[2][i])), state[3][i]), state[0][i]);
+        //Third Row Value Change
+        state2 = ffadd(ffadd(ffadd(ffmultiply(0x02, state[2][i]), ffmultiply(0x03, state[3][i])), state[0][i]), state[1][i]);
+        //Fourth Row Value Change
+        state3 = ffadd(ffadd(ffadd(ffmultiply(0x02, state[3][i]), ffmultiply(0x03, state[0][i])), state[1][i]), state[2][i]);
+
+        //Assign Calculated Values
+        state[0][i] = state0;
+        state[1][i] = state1;
+        state[2][i] = state2;
+        state[3][i] = state3;
+    }
 }
 
 // This transformation adds a round key to the State using XOR.
-void addRoundKey(uint32_t **state)
+void addRoundKey(uint8_t **state)
 {
 
 }
 
 // This transformation substitutes each byte in the State with its corresponding value from the inverse S-Box,
 // thus reversing the effect of a subBytes() operation.
-void invSubBytes(uint32_t **state)
+void invSubBytes(uint8_t **state)
 {
 
 }
 
 // This transformation performs the inverse of shiftRows() on each row in the State
-void invShiftRows(uint32_t **state)
+void invShiftRows(uint8_t **state)
 {
 
 }
 
 // This transformation is the inverse of mixColumns
-void invMixColumns(uint32_t **state)
+void invMixColumns(uint8_t **state)
 {
 
 }
+
+//Add Cipher
+//Add InvCipher
 
 int main (int argc, char *argv[]) 
 {
